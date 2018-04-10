fd635cb0a9fcae0622ffdccfd970f96b5fb41bc7
Andrew Wilson <anddwilson@gmail.com>
2018-04-10 10:43:24 -0400

    Added react-table and populated with data from Google Sheet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,61 @@
 import React from "react";
+import matchSorter from 'match-sorter'
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+
 
 export default ({ data }) => (
   <div>
-    <h4>{data.allGoogleSheetFormResponses1Row.totalCount} Programs</h4>
-    <ul>
-    {data.allGoogleSheetFormResponses1Row.edges.map(({ node }) => (
-        <li>{node.nameOfProgram} | {node.organization}</li>
-    ))}
-    </ul>
+    <ReactTable
+      data={data.allGoogleSheetFormResponses1Row.edges.map(x => x.node)}
+      columns={[
+        {
+          Header: "Name",
+          columns: [
+            {
+              Header: "First Name",
+              accessor: "firstName"
+            },
+            {
+              Header: "Last Name",
+              id: "lastName",
+              accessor: d => d.lastName
+            }
+          ]
+        },
+        {
+          Header: "Info",
+          columns: [
+            {
+              Header: "Experience",
+              accessor: "experienceLevel"
+            },
+            {
+              Header: "Sector",
+              accessor: "sector"
+            },
+            {
+              Header: "Commitment",
+              accessor: "commitment"
+            },
+          ]
+        },
+        {
+          Header: 'Details',
+          columns: [
+            {
+              Header: "Program",
+              accessor: "nameOfProgram"
+            },
+            {
+              Header: "Organization",
+              accessor: "organization"
+            }
+          ]
+        }
+      ]}
+      defaultPageSize={15}
+    />
   </div>
 );
 
@@ -21,7 +69,7 @@ export const query = graphql`
           firstName: firstname
           lastName: lastname
           position
-          email 
+          email
           experienceLevel: pleasecheckallthatapplytothetargetlevelofeducationandorcareerexperienceyourprogramseekstoattract
           organization
           nameOfProgram: nameofprogram
