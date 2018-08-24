00ccedf64c1b6d8237eeb19b7a03d73519d8b715
Andrew Wilson <5093707+andwilson@users.noreply.github.com>
2018-08-24 13:41:25 -0400

    Removed header and footer to leave just the table

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,40 +9,10 @@ import Footer from "../components/Footer"
 import Navigation from "../components/Navigation"
 
 const Wrapper = styled.div`
-  display: grid;
-  min-height: 100vh;
-  grid-template-rows: 60px auto 60px;
-  grid-template-areas: "navigation"
-                       "content"
-                       "footer";
-  grid-gap: 10px;
-  margin: auto;
-  padding: 0 10px 0 10px;
-  max-width: 960px;
-`;
-
-const Content = styled.div`
-  grid-area: content;
-`;
-
-const NavigationArea = styled(Navigation)`
-  grid-area: navigation;
-`;
-
-const FooterArea = styled(Footer)`
-  grid-area: footer;
 `;
 
 export default ({ children}) => (
   <Wrapper>
-
-    <NavigationArea />
-
-    <Content>
       {children()}
-    </Content>
-
-    <FooterArea />
-
   </Wrapper>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,17 @@
 import React from "react";
-import matchSorter from 'match-sorter'
+import matchSorter from "match-sorter";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 import styled from "styled-components";
 
-const P = styled.p`
-  margin-bottom: 1em;
-`;
-
 export default ({ data }) => (
   <div>
-    <P>As a part of our work to strengthen the field, the Robert Sterling Clark Foundation is creating a resource directory of leadership development programs available to those living, working, and leading in New York City. It is our hope that the resource will serve both organizations and individuals interested in leadership development. We are working to create an easily-navigated and comprehensible platform by collecting and sharing information on leadership development programs and presenting them in a standardized format.</P>
-    <P>There are currently {data.allGoogleSheetFormResponses1Row.totalCount} programs. You can discover programs by <i>filtering</i> and <i>sorting</i>, and can click on each to learn more.</P>
     <ReactTable
       data={data.allGoogleSheetFormResponses1Row.edges.map(x => x.node)}
       filterable
       defaultFilterMethod={(filter, row) =>
-            String(row[filter.id]) === filter.value}
+        String(row[filter.id]) === filter.value
+      }
       columns={[
         {
           Header: "Leadership Development Programs",
@@ -25,35 +20,35 @@ export default ({ data }) => (
               Header: "Program",
               accessor: "nameOfProgram",
               filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, { keys: ["nameOfProgram"] }),
+                matchSorter(rows, filter.value, { keys: ["nameOfProgram"] }),
               filterAll: true
             },
             {
               Header: "Organization",
               accessor: "organization",
               filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, { keys: ["organization"] }),
+                matchSorter(rows, filter.value, { keys: ["organization"] }),
               filterAll: true
             },
             {
               Header: "Level",
               accessor: "experienceLevel",
               filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, { keys: ["experienceLevel"] }),
+                matchSorter(rows, filter.value, { keys: ["experienceLevel"] }),
               filterAll: true
             },
             {
               Header: "Focus",
               accessor: "focus",
               filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, { keys: ["focus"] }),
+                matchSorter(rows, filter.value, { keys: ["focus"] }),
               filterAll: true
             },
             {
               Header: "Commitment",
               accessor: "commitment",
               filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, { keys: ["commitment"] }),
+                matchSorter(rows, filter.value, { keys: ["commitment"] }),
               filterAll: true
             }
           ]
@@ -64,9 +59,7 @@ export default ({ data }) => (
       SubComponent={row => {
         return (
           <div style={{ padding: "20px" }}>
-            <em>
-              Program details!!!
-            </em>
+            <em>Program details!!!</em>
           </div>
         );
       }}
